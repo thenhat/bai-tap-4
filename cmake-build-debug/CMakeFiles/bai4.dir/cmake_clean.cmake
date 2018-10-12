6f1433ee022dd62fc23bc2ea984881143fc07d53
thenhat <NhatNTTH1809023@fpt.edu.vn>
2018-10-12 21:02:44 +0700

    bai-tap-4

---
+++ cmake-build-debug/CMakeFiles/bai4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bai4.dir/main.c.o"
+  "bai4.pdb"
+  "bai4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bai4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float tinhccanhhuyen(int a, int b) {
+    return (float) sqrt(a * a + b * b);
+}
+
+int main() {
+    int a, b;
+    printf("nhap vao canh thu nhat cua tam giac vuong: ");
+    scanf("%d", &a);
+    printf("nhap vao canh thu hai cua tam giac vuong: ");
+    scanf("%d", &b);
+    if (a <= 0 || b <= 0) {
+        printf("canh tam giac khong the nho hon 0");
+        exit;
+    }
+    printf("Chieu dai canh huyen la: %.2f\n", tinhccanhhuyen(a, b));
+    return 0;
+}
